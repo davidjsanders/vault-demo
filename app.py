22175d7bf4d0b59b3a99714d185b8368b2387d90
David Sanders <dsanderscanada@gmail.com>
2016-10-16 11:02:17 -0400

    Remove support to pass token via args (insecure).

--- app.py
+++ app.py
@@ -8,12 +8,6 @@ vault = None
 
 parser = argparse.ArgumentParser(description='Process optional tokens')
 parser.add_argument(
-    '--token',
-    dest='token',
-    default=None,
-    help='Pass the Vault authentication token via the command line.'
-)
-parser.add_argument(
     '--wrapped-token',
     dest='wrapped_token',
     default=None,
@@ -22,20 +16,16 @@ parser.add_argument(
 args = parser.parse_args()
 
 try:
-    if args.token is None:
-        if args.wrapped_token is None:
-            f = open('authentication_file.json', 'r')
-            _auth_dict = json.load(f)
-            if 'auth' in _auth_dict:
-                auth = _auth_dict['auth']
-            elif 'wrapped' in _auth_dict:
-                wrapped = _auth_dict['wrapped']
-            f.close()
-        else:
-            wrapped = args.wrapped_token
+    if args.wrapped_token is None:
+        f = open('authentication_file.json', 'r')
+        _auth_dict = json.load(f)
+        if 'auth' in _auth_dict:
+            auth = _auth_dict['auth']
+        elif 'wrapped' in _auth_dict:
+            wrapped = _auth_dict['wrapped']
+        f.close()
     else:
-        auth = args.token
-        wrapped = None
+        wrapped = args.wrapped_token
 except Exception as e:
     Logger.log('Exception! {0}'.format(repr(e)))
     exit(1)
