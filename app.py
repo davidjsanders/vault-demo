291a02e21ad696f146f47f851308fc820b00a006
David Sanders <dsanderscanada@gmail.com>
2016-10-17 12:39:43 -0400

    Add debug support.

--- app.py
+++ app.py
@@ -57,9 +57,11 @@ _port = args.port
 _auth_file = args.auth_file
 _debug = args.debug
 
-# Check if a port number has been passed as a parameter
 if _debug:
-    logging.debug('Checking port is properly defined.')
+    logging.getLogger().setLevel(logging.DEBUG)
+
+# Check if a port number has been passed as a parameter
+logging.debug('Checking port is properly defined.')
 
 if _port is not None:
     _success = False
@@ -79,13 +81,11 @@ if _port is not None:
 
     if _port < 1:
         raise ValueError('Port number, if provided, must be greater than zero or None.')
-    if _debug:
-        logging.debug('Port IS properly defined.')
+    logging.debug('Port IS properly defined.')
 
 # Check if an auth file has been passed and that it exists
 if _auth_file is not None:
-    if _debug:
-        logging.debug('Checking authentication file is properly defined.')
+    logging.debug('Checking authentication file is properly defined.')
 
     if not isinstance(_auth_file, str):
         raise ValueError('Auth file, if provided, must be a filename (i.e. a string).')
@@ -105,8 +105,7 @@ if _auth_file is not None:
         raise fnf_error
     t_auth = json.load(_auth_file_handle)
     _auth_file_handle.close()
-    if _debug:
-        logging.debug('Authentication file was found and opened.')
+    logging.debug('Authentication file was found and opened.')
 
     _wrapped = t_auth.get('wrapped-token', None)
     if _wrapped is None:
@@ -116,12 +115,13 @@ if _auth_file is not None:
 
         logging.error(_error_text)
         raise ValueError(_error_text)
-    if _debug:
-        logging.debug('Authentication file was valid.')
+    logging.debug('Authentication file was valid.')
 
+logging.debug('Connecting to server {0} on port {1}'.format(_server, _port))
 vault = VaultServer(name=_server, port=_port)
 
 try:
+    logging.debug('Authenticating to server with wrapped token.')
     vault.authenticate(wrapped_token=_wrapped)
 except requests.ConnectionError as ce:
     logging.error('A connection error occurred; is the vault server information correct?')
@@ -133,6 +133,7 @@ except Exception as e:
     logging.error('Exception! {0}'.format(repr(e)))
     raise
 
+logging.debug('Authentication succeeded. Fetching value "foo"')
 try:
     foo = vault.read_kv_secret(secret='foo')
     print('foo: {0}'.format(foo))
@@ -147,3 +148,5 @@ except hvac.exceptions.Forbidden as f:
         )
     logging.error(_error_text)
     raise
+
+logging.debug('Program ends.')
